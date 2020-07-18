7f8d7761a2f6ea9c6da852138b310e4a8b5fb4a9
MaheemaPavithrani <maheemapavithrani@gmail.com>
2020-07-18 20:27:35 +0530

    created login and signup ui

---
+++ src/component/index.js
@@ -0,0 +1,9 @@
+import RoundCornerButton from "./button/RoundCornerButton";
+import InputField from "./input";
+import Logo from "./logo";
+
+export {
+  RoundCornerButton,
+  InputField,
+  Logo,
+};
--- src/container/Login/index.js
+++ src/container/Login/index.js
@@ -1,10 +1,61 @@
-import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import React, { useState } from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+import {globalStyle, color} from '../../utility';
+import {Logo, InputField, RoundCornerButton} from '../../component';
 
 const Login = ({navigation}) => {
+  const [credentials, setCredentials] = useState({
+    email: '',
+    password: '',
+  });
+  const {email,password} = credentials;
+
+  onLoginPress = () =>{
+    if (!email){
+      alert('Email is required');
+    }
+    else if(!password){
+      alert('Password is required');
+    }
+    else{
+      alert(JSON.stringify(credentials));
+
+    }
+  };
+
+  const handleOnChange = (name, value) =>{
+    setCredentials({
+      ...credentials,
+      [name]: value,
+    });
+  };
+
   return(
-    <SafeAreaView>
-      <Text onPress={()=>navigation.navigate('SignUp')} >Login </Text>
+    <SafeAreaView style={[globalStyle.flex1, {backgroundColor: color.BLACK}]
+    }>
+      <View style={[globalStyle.containerCentered]}>
+        <Logo />
+      </View>
+      
+      <View style={[globalStyle.flex2, globalStyle.sectionCentered]}>
+        <InputField placeholder="Enter email" value={email}
+          onChangeText={(text)=>handleOnChange('email', text)} 
+        />
+        <InputField placeholder="Enter password" secureTextEntry={true} value={password }
+          onChangeText={(text)=>handleOnChange('password', text)}
+         /> 
+        <RoundCornerButton title="Login" onPress={()=> onLoginPress()} />
+        <Text
+          style={{
+            fontSize: 28,
+            fontWeight: 'bold',
+            color: color.LIGHT_GREEN,
+          }}
+          onPress={() => navigation.navigate('SignUp')}>
+            Sign Up
+
+        </Text>
+      </View>
     </SafeAreaView>
 
   );
