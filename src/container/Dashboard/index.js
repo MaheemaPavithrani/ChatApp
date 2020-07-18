0a0ad086738519f7c2c25dadf79c29e9f55ade2a
MaheemaPavithrani <maheemapavithrani@gmail.com>
2020-07-18 12:07:40 +0530

    dashboard logout alert created

--- src/container/Dashboard/index.js
+++ src/container/Dashboard/index.js
@@ -1,7 +1,37 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, { useLayoutEffect } from 'react';
+import {View, Text, Alert} from 'react-native';
+import {color} from '../../utility';
+import SimpleLineIcon from 'react-native-vector-icons/SimpleLineIcons';
 
-const Dashboard = () => {
+const Dashboard = ({navigation}) => {
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <SimpleLineIcon 
+          name="logout" 
+          size={26} 
+          color={color.WHITE}
+          style={{right:10}}
+          onPress={()=> 
+            Alert.alert(
+              'logout', 
+              'Are you sure to log out',
+          [
+            {
+              text: 'Yes',
+              onPress:()=>alert('logged out')
+            },
+            {
+              text: 'No'
+            }
+          ],{
+            cancelabel:false
+          }
+          )}
+          /> 
+      ),
+    });
+  }, [navigation]);
   return(
     <View>
       <Text>Dashboard</Text>
